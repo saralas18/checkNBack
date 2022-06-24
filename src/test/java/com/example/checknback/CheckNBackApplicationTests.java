72e9a14986625629e546a4695deb356a465936aa
unknown <saralasnami1998@gmail.com>
2022-06-24 14:17:45 +0200

    Init

---
+++ src/test/java/com/example/checknback/CheckNBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checknback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckNBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
